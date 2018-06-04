3dd67f75845c4f7d8832b71328d863d527ea034b
Oleh Borysenko <olegstyle1@gmail.com>
2018-06-04 14:41:22 +0200

    add polish language

---
+++ resources/lang/pl/main.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: royalweb
+ * Date: 2/15/17
+ * Time: 16:17
+ */
+
+return [
+    'skills' => 'Umiejętności',
+    'about_me' => 'O mnie',
+    'portfolio' => 'Portfolio',
+    'contacts' => 'Kontakty',
+    'my_skills' => 'Moje umiejętności',
+    'all_skills' => 'Wszystkie umiejętności',
+    'show_all' => 'Jeszcze',
+    'more' => 'Jeszcze',
+    'my_portfolio' => 'Moje portfolio',
+    'all_projects' => 'Wszystkie projekty',
+    'gist' => 'Sens',
+    'description' => 'Opis',
+    'look' => 'Pogląd',
+    'date_start' => 'Data rozpoczęcia',
+    'date_end' => 'Data końcowa',
+    'technology_stack' => 'Stos technologii',
+    'name_empty' => 'Didn\'t set name',
+    'email_empty' => 'Didn\'t set email',
+    'email_invalid' => 'Email\'s invalid',
+    'message_empty' => 'Didn\'t set message',
+    'send_success' => 'Wysłano pomyślnie',
+    'start_sending' => 'Wysyłanie...',
+    'message' => 'Wiadomość',
+    'send' => 'Wysłać',
+];
