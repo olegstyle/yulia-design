c0e303e61f8b4a50e299a7241cbc54d0736c6ecf
Oleh Borysenko <oleg.borisenko@morefromit.com>
2019-03-14 11:07:05 +0100

    update packages resolve version conflicts

--- config/voyager.php
+++ config/voyager.php
@@ -52,7 +52,7 @@ return [
     */
 
     'storage' => [
-        'disk' => 'public', // include trailing slash, like 'my_folder/'
+        'disk' => env('FILESYSTEM_DRIVER', 'public'), // include trailing slash, like 'my_folder/'
     ],
 
     /*
@@ -65,9 +65,29 @@ return [
     */
 
     'database' => [
+        'autoload_migrations' => false, // true
         'tables' => [
             'hidden' => [], // database tables that are hidden from the admin panel
         ],
     ],
 
+    'media' => [
+        // The allowed mimetypes to be uploaded through the media-manager.
+        'allowed_mimetypes' => [
+            'image/jpeg',
+            'image/png',
+            'image/gif',
+            'image/bmp',
+            'video/mp4',
+        ],
+
+        //Path for media-manager. Relative to the filesystem.
+        'path'                => '/',
+        'show_folders'        => true,
+        'allow_upload'        => true,
+        'allow_move'          => true,
+        'allow_delete'        => true,
+        'allow_create_folder' => true,
+        'allow_rename'        => true,
+    ],
 ];
